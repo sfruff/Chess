ec5d1de29ac58e969f3c9dd7ab671ca7b7bf14d4
Cx3n1 <lukagobronidze@gmail.com>
2022-06-28 20:03:47 +0400

    added update function working on win conditions

--- app/src/main/java/com/tsu/android/chess/ChessGame.kt
+++ app/src/main/java/com/tsu/android/chess/ChessGame.kt
@@ -6,7 +6,7 @@ import com.tsu.android.chess.data.Player
 import com.tsu.android.chess.data.Square
 
 object ChessGame {
-
+    private var turn : Player = Player.WHITE;
     private var whiteWinCount = 0
     private var blackWinCount = 0
 
@@ -16,6 +16,65 @@ object ChessGame {
         reset()
     }
 
+    fun updateOnMove(){
+        nextTurn()
+
+        checkForResults()
+
+    }
+
+    private fun checkForResults() {
+        val winner : Player? = checkWhoWins()
+
+        if(winner == Player.WHITE) whiteWins()
+        else if (winner == Player.BLACK) blackWins()
+        else if (checkForDraw()) draw()
+
+    }
+
+    private fun draw() {
+
+
+    }
+
+    private fun checkForDraw(): Boolean {
+        piecesBox.forEach{
+            if(it.player == turn){
+                if(turn == Player.WHITE && isTherePossibilityToMoveForWhite(it)){
+                    return true
+                } else if (canPawnMoveToRow(it)){
+                    return true
+                }
+            }
+        }
+        return false
+    }
+
+    /***
+     * @param rowFromIt increment this value from current row to determine row on which you want to check if pawn can move
+     */
+    private fun canPawnMoveToRow(it: ChessPiece, rowFromIt: Int): Boolean {
+        val col = it.col
+        val row = it.row
+        val currSquare = Square(col, row);
+
+        for (incCol in -1 until 2){
+            val newCol = col + incCol
+            val newRow = row + rowFromIt
+            if(canPawnMove(currSquare, Square(newCol, newRow)))
+                return true
+        }
+        return false
+    }
+
+    private fun checkWhoWins(): Player? {
+        piecesBox.forEach{
+            if(it.player == Player.WHITE && it.row == BOARD_SIZE - 1) return Player.WHITE
+            else if(it.player == Player.BLACK && it.row == 0) return Player.BLACK
+        }
+        return null
+    }
+
     fun clear() {
         piecesBox.clear()
     }
@@ -23,7 +82,7 @@ object ChessGame {
     fun reset() {
         whiteWinCount = 0
         blackWinCount = 0
-        resetPieces();
+        resetPieces()
     }
 
     fun resetPieces(){
@@ -116,50 +175,8 @@ object ChessGame {
                  (pieceAt(to) != null && (from.col + 1 == to.col || from.col - 1 == to.col))
              )
 
-    //TODO:Why do we need these???
-    fun pgnBoard(): String {
-        var desc = " \n"
-        desc += "  a b c d e f g h\n"
-        for (row in (BOARD_SIZE - 1) downTo 0) {
-            desc += "${row + 1}"
-            desc += boardRow(row)
-            desc += " ${row + 1}"
-            desc += "\n"
-        }
-        desc += "  a b c d e f g h"
-
-        return desc
+    private fun nextTurn(){
+        if(turn == Player.WHITE) turn = Player.BLACK
+        else turn = Player.WHITE
     }
-
-    override fun toString(): String {
-        var desc = " \n"
-        for (row in (BOARD_SIZE - 1) downTo 0) {
-            desc += "$row"
-            desc += boardRow(row)
-            desc += "\n"
-        }
-        desc += "  0 1 2 3 4 5 6 7"
-
-        return desc
-    }
-
-    private fun boardRow(row: Int): String {
-        var desc = ""
-        for (col in 0 until BOARD_SIZE) {
-            desc += " "
-            desc += pieceAt(col, row)?.let {
-                val white = it.player == Player.WHITE
-                when (it.chessman) {
-                    Chessman.KING -> if (white) "k" else "K"
-                    Chessman.QUEEN -> if (white) "q" else "Q"
-                    Chessman.BISHOP -> if (white) "b" else "B"
-                    Chessman.ROOK -> if (white) "r" else "R"
-                    Chessman.KNIGHT -> if (white) "n" else "N"
-                    Chessman.PAWN -> if (white) "p" else "P"
-                }
-            } ?: "."
-        }
-        return desc
-    }
-    //what???
 }
