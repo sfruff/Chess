346d1f2d73590909532da9f772ede450c8df1dd9
Cx3n1 <lukagobronidze@gmail.com>
2022-06-28 22:09:51 +0400

    updated winning and losing checks and added score point space in activity

--- app/src/main/java/com/tsu/android/chess/ChessGame.kt
+++ app/src/main/java/com/tsu/android/chess/ChessGame.kt
@@ -7,7 +7,7 @@ import com.tsu.android.chess.data.Player
 import com.tsu.android.chess.data.Square
 
 object ChessGame {
-    private var turn : Player = Player.WHITE;
+    private var turn: Player = Player.WHITE;
     private var whiteWinCount = 0
     private var blackWinCount = 0
 
@@ -17,67 +17,14 @@ object ChessGame {
         reset()
     }
 
-    fun updateOnMove(){
+    fun updateOnMove() {
         nextTurn()
-
         checkForResults()
-
-    }
-
-    private fun checkForResults() {
-        val winner : Player? = checkWhoWins()
-
-        if(winner == Player.WHITE) whiteWins()
-        else if (winner == Player.BLACK) blackWins()
-        else if (checkForDraw()) draw()
-
-    }
-
-    private fun draw() {
-
-
-    }
-
-    private fun checkForDraw(): Boolean {
-        piecesBox.forEach{
-            if(it.player == turn){
-                if(turn == Player.WHITE && isTherePossibilityToMoveForWhite(it)){
-                    return true
-                } else if (canPawnMoveToRow(it)){
-                    return true
-                }
-            }
-        }
-        return false
-    }
-
-    /***
-     * @param rowFromIt increment this value from current row to determine row on which you want to check if pawn can move
-     */
-    private fun canPawnMoveToRow(it: ChessPiece, rowFromIt: Int): Boolean {
-        val col = it.col
-        val row = it.row
-        val currSquare = Square(col, row);
-
-        for (incCol in -1 until 2){
-            val newCol = col + incCol
-            val newRow = row + rowFromIt
-            if(canPawnMove(currSquare, Square(newCol, newRow)))
-                return true
-        }
-        return false
-    }
-
-    private fun checkWhoWins(): Player? {
-        piecesBox.forEach{
-            if(it.player == Player.WHITE && it.row == BOARD_SIZE - 1) return Player.WHITE
-            else if(it.player == Player.BLACK && it.row == 0) return Player.BLACK
-        }
-        return null
     }
 
     fun clear() {
         piecesBox.clear()
+        turn = Player.WHITE
     }
 
     fun reset() {
@@ -86,7 +33,7 @@ object ChessGame {
         resetPieces()
     }
 
-    fun resetPieces(){
+    fun resetPieces() {
         clear()
         for (i in 0 until BOARD_SIZE) {
             addPiece(
@@ -123,23 +70,15 @@ object ChessGame {
             return false
         }
         pieceAt(from) ?: return false
+        if(pieceAt(from)?.player != turn) return false
         return canPawnMove(from, to)
     }
 
     fun movePiece(from: Square, to: Square) {
         if (canMove(from, to)) {
             movePiece(from.col, from.row, to.col, to.row)
+            updateOnMove()
         }
-    }
-
-    fun whiteWins(){
-        whiteWinCount++
-        resetPieces()
-    }
-
-    fun blackWins(){
-        blackWinCount++
-        resetPieces()
     }
 
     private fun movePiece(fromCol: Int, fromRow: Int, toCol: Int, toRow: Int) {
@@ -166,18 +105,87 @@ object ChessGame {
         return null
     }
 
+
     //**Utility**\\
     private fun canPawnMove(from: Square, to: Square): Boolean =
         ((pieceAt(from)?.player == Player.WHITE && from.row + 1 == to.row) ||
-         (pieceAt(from)?.player == Player.BLACK && from.row - 1 == to.row))
-             &&
-             (
-                 (pieceAt(to) == null && from.col == to.col) ||
-                 (pieceAt(to) != null && (from.col + 1 == to.col || from.col - 1 == to.col))
-             )
+                (pieceAt(from)?.player == Player.BLACK && from.row - 1 == to.row))
+                &&
+                (
+                        (pieceAt(to) == null && from.col == to.col) ||
+                                (pieceAt(to) != null && (from.col + 1 == to.col || from.col - 1 == to.col))
+                        )
 
-    private fun nextTurn(){
-        if(turn == Player.WHITE) turn = Player.BLACK
+    private fun nextTurn() {
+        if (turn == Player.WHITE) turn = Player.BLACK
         else turn = Player.WHITE
     }
+
+    private fun draw() {
+        //TODO: make announcement
+
+        resetPieces()
+    }
+
+    private fun whiteWins() {
+        //TODO: make announcement
+        whiteWinCount++
+        resetPieces()
+    }
+
+    private fun blackWins() {
+        //TODO: make announcement
+        blackWinCount++
+        resetPieces()
+    }
+
+    private fun checkForResults() {
+        val winner: Player? = checkWhoWins()
+
+        if (winner == Player.WHITE) whiteWins()
+        else if (winner == Player.BLACK) blackWins()
+        else if (checkForDraw()) draw()
+
+    }
+
+    private fun checkForDraw(): Boolean {
+        piecesBox.forEach {
+            if (it.player == turn) {
+                if (
+                    (turn == Player.WHITE && canPawnMoveToRow(it, 1))
+                    || canPawnMoveToRow(it, -1)
+                ) {
+                    return false
+                }
+            }
+        }
+        return true
+    }
+
+    private fun checkWhoWins(): Player? {
+        piecesBox.forEach {
+            if (it.player == Player.WHITE && it.row == BOARD_SIZE - 1) return Player.WHITE
+            else if (it.player == Player.BLACK && it.row == 0) return Player.BLACK
+        }
+        return null
+    }
+
+    /***
+     * @param rowFromIt increment this value from current row to determine row on which you want to check if pawn can move
+     */
+    private fun canPawnMoveToRow(it: ChessPiece, rowFromIt: Int): Boolean {
+        val col = it.col
+        val row = it.row
+        val currSquare = Square(col, row);
+
+        for (incCol in -1 until 2) {
+            val newCol = col + incCol
+            val newRow = row + rowFromIt
+            if (canPawnMove(currSquare, Square(newCol, newRow))){
+                return true
+            }
+        }
+        return false
+    }
+
 }
